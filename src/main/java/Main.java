beb8820de6e557bc9b91e151af5bad7a5b0d25af
Patrick Hammarstedt <patrick.hammarstedt@hm.com>
2021-04-13 15:55:12 +0200

    initial creation

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = terminalFactory.createTerminal();
+        
+    }
+}
