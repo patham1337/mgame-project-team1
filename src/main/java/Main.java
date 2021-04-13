d06c6d5905c4c840146dd5f8b314fc3aa28f5e06
Hakan Danielsson <hakan.danielsson@hm.com>
2021-04-13 16:32:29 +0200

    Add hakan branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
+//håkan
     public static void main(String[] args) throws Exception {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
