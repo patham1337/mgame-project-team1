56c1225744c0893a521a572896c5e12d6cefc46a
Annica Landbü <annica.landbu@hm.com>
2021-04-16 15:51:43 +0200

    Added more walls

--- src/main/java/Maze.java
+++ src/main/java/Maze.java
@@ -12,10 +12,15 @@ public class Maze {
    public Maze(Terminal terminal) throws IOException {
        terminal.setForegroundColor(TextColor.ANSI.GREEN);
 
-       maze.add(new Wall(terminal, 1,5, 20, 5)); //Linje 1 övre
-       maze.add(new Wall(terminal, 1, 2, 25, 2)); //Linje 1 nedre
+       maze.add(new Wall(terminal, 1, 2, 25, 2)); //Linje 1 övre
+      // maze.add(new Wall(terminal, 1,5, 20, 5)); //Linje 1 nedre
+       maze.add(new Wall(terminal, 1, 5, 5, 5)); //Linje 1 nedre
+       maze.add(new Wall(terminal, 10, 5, 20, 5)); //Linje 1 nedre
+
        maze.add(new Wall(terminal, 20,5, 20, 15)); //Linje 2 vänster
-       maze.add(new Wall(terminal, 25,2, 25, 12)); //Linje 2 höger
+       //maze.add(new Wall(terminal, 25,2, 25, 12)); //Linje 2 höger
+       maze.add(new Wall(terminal, 25,2, 25, 5)); //Ny rad
+       maze.add(new Wall(terminal, 25,8, 25, 12)); //Ny rad
 
        maze.add(new Wall(terminal, 25,12, 35, 12)); //Linje 3 övre
        maze.add(new Wall(terminal, 20,15, 30, 15)); //Linje 3 nedre
@@ -29,11 +34,21 @@ public class Maze {
        maze.add(new Wall(terminal, 60,4, 70, 4));
        maze.add(new Wall(terminal, 70,4, 70, 20));
        maze.add(new Wall(terminal, 75,2, 75, 20));
-       maze.add(new Wall(terminal, 55,2, 55, 20));
+       //maze.add(new Wall(terminal, 55,2, 55, 20));
+       maze.add(new Wall(terminal, 55,2, 55, 5));//Ny rad
+       maze.add(new Wall(terminal, 55,8, 55, 20)); //Ny rad
+
        maze.add(new Wall(terminal, 60,4, 60, 25));
 
+       //Nya rader
+       maze.add(new Wall(terminal, 25,5, 55, 5)); //Linje övre
+       maze.add(new Wall(terminal, 25, 8, 55, 8)); //Linje nedre
 
+       maze.add(new Wall(terminal, 5,5, 5, 20)); //Linje vänster
+       maze.add(new Wall(terminal, 10,5, 10, 17)); //Linje höger
 
+       maze.add(new Wall(terminal, 10, 17, 30, 17)); //Linje övre
+       maze.add(new Wall(terminal, 5,20, 30, 20)); //Linje  nedre
 
        terminal.flush();
    }
